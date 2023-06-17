6e83bb52f141ebb06fea99de8a4426b1b2b1c616
Vicente Santos <vicentedpsantos@gmail.com>
2023-06-17 20:04:07 +0100

    Add grocery list

---
+++ lib/widgets/grocery_list.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+import 'package:shopping_list/data/dummy_items.dart';
+
+class GroceryList extends StatelessWidget {
+  GroceryList({super.key});
+
+  @override
+  Widget build(context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Your Groceries',
+        ),
+      ),
+      body: ListView.builder(
+        itemCount: groceryItems.length,
+        itemBuilder: (ctx, index) => ListTile(
+          title: Text(groceryItems[index].name),
+          leading: Container(
+            width: 24,
+            height: 24,
+            color: groceryItems[index].category.color,
+          ),
+          trailing: Text(
+            groceryItems[index].quantity.toString(),
+          ),
+        ),
+      ),
+    );
+  }
+}
