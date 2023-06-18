e9c560b7d64088928148468581eaf680e69b12f6
Vicente Santos <vicentedpsantos@gmail.com>
2023-06-18 20:44:53 +0100

    Add fallback when there are no items

--- lib/widgets/grocery_list.dart
+++ lib/widgets/grocery_list.dart
@@ -31,19 +31,12 @@ class _GroceryListState extends State<GroceryList> {
 
   @override
   Widget build(context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          'Your Groceries',
-        ),
-        actions: [
-          IconButton(
-            onPressed: _addItem,
-            icon: const Icon(Icons.add),
-          ),
-        ],
-      ),
-      body: ListView.builder(
+    Widget content = const Center(
+      child: Text('No items added yet...'),
+    );
+
+    if (_groceryItems.isNotEmpty) {
+      content = ListView.builder(
         itemCount: _groceryItems.length,
         itemBuilder: (ctx, index) => ListTile(
           title: Text(_groceryItems[index].name),
@@ -56,7 +49,22 @@ class _GroceryListState extends State<GroceryList> {
             _groceryItems[index].quantity.toString(),
           ),
         ),
+      );
+    }
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Your Groceries',
+        ),
+        actions: [
+          IconButton(
+            onPressed: _addItem,
+            icon: const Icon(Icons.add),
+          ),
+        ],
       ),
+      body: content,
     );
   }
 }
