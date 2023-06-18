68e07150aed330a10bcb2f4b34e56398562adc80
Vicente Santos <vicentedpsantos@gmail.com>
2023-06-18 19:45:31 +0100

    Start creating a form using Form() and TextFormField()

--- lib/widgets/new_item.dart
+++ lib/widgets/new_item.dart
@@ -16,7 +16,21 @@ class _NewItemState extends State<NewItem> {
       ),
       body: Padding(
         padding: const EdgeInsets.all(12),
-        child: Text('The form'),
+        child: Form(
+          child: Column(
+            children: [
+              TextFormField(
+                maxLength: 50,
+                decoration: InputDecoration(
+                  label: Text('Name'),
+                ),
+                validator: (value) {
+                  return 'Demo...';
+                },
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
